82298b134f3e1648c2123e5216900365e22407e2
Sergio Rodrigo <rodrigosergio93@gmail.com>
2026-02-26 17:37:55 -0400

    preparing to deploy

--- src/database.py
+++ src/database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from settings import settings
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./db_test.db"
-# SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:otbcentralizadoqwerty@db.vnuioejzhnwuokpzwnqn.supabase.co:5432/postgres"
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres.vnuioejzhnwuokpzwnqn:otbcentralizadoqwerty@aws-0-us-east-2.pooler.supabase.com:5432/postgres"
-
+SQLALCHEMY_DATABASE_URL = settings.db_url
 
 engine = create_engine(
   SQLALCHEMY_DATABASE_URL,  
--- src/settings.py
+++ src/settings.py
@@ -7,4 +7,3 @@ class Settings (BaseSettings):
 
 
 settings = Settings()
-
