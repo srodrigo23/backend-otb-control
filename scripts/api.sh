356940d80da4bc13e33c06bc3ecdc0b174a34b4e
rodres101 <rodrigosergio93@gmail.com>
2026-06-19 20:29:19 -0400

    change from pip to uv

--- scripts/api.sh
+++ scripts/api.sh
@@ -4,5 +4,6 @@ cd backend
 source .venv/bin/activate
 
 
-fastapi dev app/main.py #dev mode
+#fastapi dev app/main.py #dev mode
+uv run fastapi dev app/main.py
 #uvicorn app.main:app --reload #for production
